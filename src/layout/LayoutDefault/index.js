d8dc9de0c7711d0e089c757a7d3139ee7ee3c31a
quan <quan93909@gmail.com>
2025-09-10 21:38:40 +0700

    Làm layout (2)

--- src/layout/LayoutDefault/index.js
+++ src/layout/LayoutDefault/index.js
@@ -2,22 +2,26 @@ import { Layout } from "antd";
 import "./LayoutDefault.css";
 import LearnGrid from '../../components/LearnGrid'
 import logo from "../../images/logo.png";
+import logoFold from "../../images/logo-fold.png";
 import { MenuUnfoldOutlined, SearchOutlined} from "@ant-design/icons";
+import { useState } from "react";
 
 const { Sider, Content } = Layout;
 
 function LayoutDefault() {  
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <>
       <Layout className="layout-default">
 
         <header className="header">
-          <div className="header__logo">
-            <img src={logo} alt="Logo" />
+          <div className={"header__logo " + (collapsed && "header__logo--collapsed")}>
+            <img src={collapsed ? (logoFold) : (logo)} alt="Logo" />
           </div>
           <div className="header__nav">
             <div className="header__nav-left">
-              <div className="header__collapse">
+              <div className="header__collapse" onClick={() => setCollapsed(!collapsed)}>
                 <MenuUnfoldOutlined />
               </div>
               <div className="header__search">
@@ -33,7 +37,7 @@ function LayoutDefault() {
 
         <Layout>
 
-          <Sider className="sider">
+          <Sider className="sider" collapsed={collapsed} theme="light">
             Sider
           </Sider>
 
